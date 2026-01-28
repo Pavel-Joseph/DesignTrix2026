afd640854db6fdb32f1b40d89473367b36eabf8d
Pavel Joseph Karrie <241092.CD@rmkec.ac.in>
2026-01-28 12:40:17 +0530

    small change

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import ModelViewer from './components/ModelViewer';
+import ModelViewer from './components/ModelViewer.jsx';
 // import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Hero from "./pages/Hero";
 import App from "./pages/App";
+
 <div className="App">
   {/* Your existing navbar, sections, footer */}
   
