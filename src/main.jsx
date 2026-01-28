d1345b3fd8eaaf7159ae5ccc6555c000d29442b5
Pavel Joseph Karrie <241092.CD@rmkec.ac.in>
2026-01-28 12:34:32 +0530

    eye of sauron change

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import ModelViewer from './components/ModelViewer';
 // import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Hero from "./pages/Hero";
 import App from "./pages/App";
+function App() {
+  return (
+    <div className="App">  {/* Your existing navbar, sections, footer */}
+      {/* ... all your page content ... */}
+      
+      <ModelViewer />  {/* ← Fixed 3D on right */}
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
